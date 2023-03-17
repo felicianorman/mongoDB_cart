d376fa8cce533aa4ec44cdb4f91e5e6c9144401d
Felicia <feelicianorman@hotmail.com>
2023-03-17 11:43:59 +0100

    [bug] fixed error managment

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,38 +1,54 @@
 const Cart = require("../models/Cart");
+const { NotFoundError, BadRequestError } = require("../utils/error");
 
 exports.createCart = async (req, res) => {
-  const totalAmount = req.body.totalAmount;
+  try {
+    const totalAmount = req.body.totalAmount;
 
+    const newCart = await Cart.create({
+      totalAmount: totalAmount,
+    });
 
-  const newCart = await Cart.create({
-    totalAmount: totalAmount,
-  });
+    if (!newCart) throw new BadRequestError("Could not create cart");
 
-  if(!newCart) return new Error('Could not create cart')
-
-  return res.setHeader('Location', `http://localhost:4000/api/v1/carts/${newCart._id}`).status(201).json(newCart)
+    return res
+      .setHeader(
+        "Location",
+        `http://localhost:4000/api/v1/carts/${newCart._id}`
+      )
+      .status(201)
+      .json(newCart);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
+  }
 };
 
 exports.getCartById = async (req, res) => {
+  try {
     const cartId = req.params.cartId;
 
-    const carts = await Cart.findById(cartId)
-    if(!carts) return new Error('That cart does not exist')
+    const carts = await Cart.findById(cartId);
+    if (!carts) throw new NotFoundError('That cart does not exist')
 
-    return res.json(carts)
-}
-
+    return res.json(carts);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
+  }
+};
 
 exports.deleteCart = async (req, res) => {
+  try {
     const cartId = req.params.cartId;
 
-    const cartToDelete = await Cart.findById(cartId)
+    const cartToDelete = await Cart.findById(cartId);
 
-    if(!cartToDelete) return res.sendStatus(404)
+    if (!cartToDelete) throw new NotFoundError("That cart does not exist");
 
-    const response = await cartToDelete.delete()
-    console.log(response)
+    const response = await cartToDelete.delete();
+    console.log(response);
 
-    return res.sendStatus(204)
-}
-
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
+  }
+};
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,81 +1,101 @@
 const Cart = require("../models/Cart");
 const Product = require("../models/Product");
+const { NotFoundError, BadRequestError } = require('../utils/error')
 
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+  try {
+    const products = await Product.find();
 
-  if (!products) return new Error("Could not find products");
-  return res.send(products);
+    if (!products) throw new NotFoundError("Could not find products");
+    return res.send(products);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
+  }
 };
 
 exports.getProductById = async (req, res) => {
-  const productId = req.params.productId;
+  try {
+    const productId = req.params.productId;
 
-  const product = await Product.findById(productId);
-  if (!product) return new Error("Could not find product");
+    const product = await Product.findById(productId);
+    if (!product) throw new NotFoundError("Could not find product");
 
-  res.json(product);
+    res.json(product);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
+  }
 };
 
 exports.addProductsToCart = async (req, res) => {
-  const productId = req.params.productId;
-  const cartId = req.body.cartId;
+  try {
+    const productId = req.params.productId;
+    const cartId = req.body.cartId;
 
-  const products = await Product.findById(productId);
-  const cart = await Cart.findById(cartId);
+    const products = await Product.findById(productId);
+    const cart = await Cart.findById(cartId);
 
-  if (!products || !cart)
-    return new Error("You must provde productId and cartId");
+    if (!products || !cart)
+      throw new BadRequestError("You must provde productId and cartId");
 
-  for (let i = 0; i < cart.products.length; i++) {
-    if (cart.products[i].id === productId) {
-      cart.products[i].amount++;
-      cart.totalAmount += cart.products[i].price;
+    for (let i = 0; i < cart.products.length; i++) {
+      if (cart.products[i].id === productId) {
+        cart.products[i].amount++;
+        cart.totalAmount += cart.products[i].price;
 
-      await cart.save();
-      return res.json(cart);
+        await cart.save();
+        return res.json(cart);
+      }
     }
+
+    cart.products.push(products);
+
+    cart.totalAmount += products.price;
+
+    await cart.save();
+    console.log(cart);
+
+    return res
+      .setHeader(
+        "Location",
+        `http://localhost:4000/api/v1/products/${cart._id}`
+      )
+      .status(201)
+      .json(cart);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
   }
-
-  cart.products.push(products);
-
-  cart.totalAmount += products.price;
-
-  await cart.save();
-  console.log(cart);
-
-  return res
-    .setHeader("Location", `http://localhost:4000/api/v1/products/${cart._id}`)
-    .status(201)
-    .json(cart);
 };
 
 exports.deleteProduct = async (req, res) => {
-  const productId = req.params.productId;
-  const cartId = req.body.cartId;
+  try {
+    const productId = req.params.productId;
+    const cartId = req.body.cartId;
 
-  const cart = await Cart.findById(cartId);
+    const cart = await Cart.findById(cartId);
 
-  if(!cart) return new Error('You must provde cartId')
+    if (!cart) return new BadRequestError("You must provde cartId");
 
-  for (let i = 0; i < cart.products.length; i++) {
-    if (cart.products[i].id === productId) {
-      cart.products[i].amount--;
-      await cart.save();
-
-      if (cart.products[i].amount == 0) {
-        cart.products.splice([i], 1);
+    for (let i = 0; i < cart.products.length; i++) {
+      if (cart.products[i].id === productId) {
+        cart.products[i].amount--;
         await cart.save();
+
+        if (cart.products[i].amount == 0) {
+          cart.products.splice([i], 1);
+          await cart.save();
+        }
       }
     }
+
+    cart.totalAmount = 0;
+    for (let i = 0; i < cart.products.length; i++) {
+      cart.totalAmount += cart.products[i].price * cart.products[i].amount;
+    }
+
+    await cart.save();
+
+    return res.send(cart);
+  } catch (error) {
+    return res.status(error.statusCode || 500).json(error.message);
   }
-
-  cart.totalAmount = 0;
-  for (let i = 0; i < cart.products.length; i++) {
-    cart.totalAmount += cart.products[i].price * cart.products[i].amount;
-  }
-
-  await cart.save();
-
-  return res.send(cart);
 };
